cbbd7c98646fbb8e68623eeaae57d572246f3e6a
Reggie Thompson <reggie.thompson4@gmail.com>
2021-01-05 17:55:25 -0700

    Add initial draft of Activity class constructor

---
+++ Activity.js
@@ -0,0 +1,21 @@
+class Activity {
+  constructor(category, description, id) {
+    this.category = category;
+    this.description = description;
+    this.minutes = 0;
+    this.seconds = 0;
+    this.completed = false;
+    this.id = id;
+  }
+  startTimer() {
+
+  }
+  markComplete() {
+
+  }
+  saveToStorage() {
+
+  }
+}
+
+module.exports = Activity;
