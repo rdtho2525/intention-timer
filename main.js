f0a3ab3aee7295d836717e8854e41008d43beab0
Reggie Thompson <reggie.thompson4@gmail.com>
2021-01-08 15:04:13 -0700

    Add initial draft of create-activity functionality

--- main.js
+++ main.js
@@ -5,15 +5,28 @@ var activitySelect = document.querySelector(".activity-selection");
 var startButton = document.querySelector("#start-button");
 var activityList = document.querySelector("#activity-selector");
 var inputForm = document.querySelector("form");
+var activityForm = document.querySelector("#activityForm");
+var createActivity = document.querySelector("#createActivity");
+var timerButton = document.querySelector("#timerButton");
 
 activitySelect.addEventListener('click', function(event) {
   event.target.classList.toggle("default-color");
   event.target.firstChild.src = `./assets/${event.target.firstChild.id}-active.svg`;
 });
 
-// startButton.addEventListener('click', removeForm);
-//
-// function removeForm() {
-//   activitySelect.classList.toggle("hidden");
-//   inputForm.classList.toggle("hidden");
-// };
+startButton.addEventListener('click', startActivity);
+
+function hide(element) {
+  return element.classList.add('hidden')
+}
+
+function unhide(element) {
+  return element.classList.remove('hidden')
+}
+
+function startActivity() {
+  var activity = new Activity()
+  hide(activityForm);
+  unhide(createActivity);
+  activity.startTimer()
+};
