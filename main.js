b4cd9ec39ed90d0a9020fcb5af6ffa46ed4c7f28
Reggie Thompson <reggie.thompson4@gmail.com>
2021-01-12 18:22:45 -0700

    Refactor to fit JS style and implementation standard

--- main.js
+++ main.js
@@ -1,61 +1,53 @@
-// DOM OBJECTS-------
-var activitySelect = document.querySelector("#activity-list");
-var startButton = document.querySelector("#start-button");
-var activityCard = document.querySelector("#activity-card");
-var userGoal = document.querySelector("#goals");
-var minutes = document.querySelector("#minutes");
-var seconds = document.querySelector("#seconds");
-var goalError = document.querySelector("#goal-error-message");
-var numError = document.querySelector("#number-error-message");
-var studyBtn = document.querySelector("#study");
-var meditateBtn = document.querySelector("#meditate");
-var exerciseBtn = document.querySelector("#exercise");
-var activityTitle = document.querySelector("#activityTitle");
-var activityForm = document.querySelector("#activityForm");
-var currentActivity = document.querySelector("#currentActivity");
-var timerButton = document.querySelector("#timerButton");
-var timeRemaining = document.querySelector("#timeRemaining");
-var chosenActivity = document.querySelector("#chosenActivity")
-var logActivityButton = document.querySelector("#logActivity");
-var pastActivityDefault = document.querySelector("#past-activity-default-message");
-var pastCardList = document.querySelector("#pastActivities");
-var currentChoice = document.getElementsByName("choice");
-var pastActivityCard = document.querySelector("#pastActivity");
-var createNewActivityButton = document.querySelector("#createNewActivityButton");
+var activitySelect = document.querySelector('#activityList');
+var startButton = document.querySelector('#startButton');
+var userGoal = document.querySelector('#goals');
+var minutes = document.querySelector('#minutes');
+var seconds = document.querySelector('#seconds');
+var goalError = document.querySelector('#goalErrorMessage');
+var numError = document.querySelector('#numberErrorMessage');
+var activityTitle = document.querySelector('#activityTitle');
+var activityForm = document.querySelector('#activityForm');
+var currentActivity = document.querySelector('#currentActivity');
+var timerButton = document.querySelector('#timerButton');
+var timeRemaining = document.querySelector('#timeRemaining');
+var chosenActivity = document.querySelector('#chosenActivity')
+var logActivityButton = document.querySelector('#logActivity');
+var pastActivityDefault = document.querySelector('#pastActivityDefaultMessage');
+var pastCardList = document.querySelector('#pastActivities');
+var currentChoice = document.getElementsByName('choice');
+var createNewActivityButton = document.querySelector('#createNewActivityButton');
 
-// GLOBAL VARIABLES------
+var currentCategory = '';
 var activity;
-var currentCategory = "";
-var activityData; // I add created this global variable to hold all our activities so we have a stable data model.
+var activityData;
 
-
-// EVENT LISTENERS--------
 window.addEventListener('load', setLocalStorage);
-activitySelect.addEventListener('click', selectActivity); //needs reviewing
-activitySelect.addEventListener('click', selectOption); //needs reviewing
+activitySelect.addEventListener('click', selectActivity);
 startButton.addEventListener('click', startActivity);
 timerButton.addEventListener('click', startActivityTimer);
 logActivityButton.addEventListener('click', logActivity);
 createNewActivityButton.addEventListener('click', createNewActivity);
-// FUNCTIONS----
+
 function setLocalStorage() {
-  if (localStorage.getItem("storedActivityData") === null) {
+  if (localStorage.getItem('storedActivityData') === null) {
     activityData = [];
     var stringify = JSON.stringify(activityData);
-    localStorage.setItem("storedActivityData", stringify)
+    localStorage.setItem('storedActivityData', stringify)
   } else {
     hide(pastActivityDefault);
     unhide(pastCardList);
-    getLocalStorage();
+    displayLocalStorage();
   }
 }
 
-function getLocalStorage() {
-  var getLocalData = localStorage.getItem("storedActivityData");
+function displayLocalStorage() {
+  var getLocalData = localStorage.getItem('storedActivityData');
   var parsedLocalData = JSON.parse(getLocalData);
+
   if (!parsedLocalData.length) {
     return
   }
+
   for (i = 0; i < parsedLocalData.length; i++) {
     pastCardList.innerHTML +=
     `<li class="past-activity-card">
@@ -70,58 +62,58 @@ function getLocalStorage() {
 }
 
 function hide(element) {
-  return element.classList.add('hidden')
+  return element.classList.add('hidden');
 }
 
 function unhide(element) {
-  return element.classList.remove('hidden')
+  return element.classList.remove('hidden');
 }
 
 function hideError(element) {
-  return element.classList.add('invisible')
+  return element.classList.add('invisible');
 }
 
 function unhideError(element) {
-  return element.classList.remove('invisible')
-}
-
-function selectOption() {
-  for (i = 0; i < currentChoice.length; i++) {
-    output = document.querySelector(`#${currentChoice[i].value}`);
-    currentChoice[i].checked ? unhide(output) : hide(output);
-  }
+  return element.classList.remove('invisible');
 }
 
 function checkInput() {
   var validInput;
-  if (userGoal.value === "") {
-    unhideError(goalError)
-    validInput = false
+
+  if (userGoal.value === '') {
+    unhideError(goalError);
+    validInput = false;
   }
-  if (minutes.value === "" || seconds.value === "" || isNaN(minutes.value) || isNaN(seconds.value)) {
-    unhideError(numError)
-    inputIsGood = false
+
+  if (minutes.value === '' || seconds.value === '' || isNaN(minutes.value) || isNaN(seconds.value)) {
+    unhideError(numError);
+    validInput = false;
   } else {
     hideError(goalError);
-    hideError(numError)
-    validInput = true
+    hideError(numError);
+    validInput = true;
   }
-  return validInput
+  return validInput;
 }
 
 function selectActivity() {
+  var output;
   for (var i = 0; i < currentChoice.length; i++) {
+    output = document.querySelector(`#${currentChoice[i].value}`);
+    currentChoice[i].checked ? unhide(output) : hide(output);
+
     if (currentChoice[i].checked === true) {
       currentCategory = currentChoice[i].value;
       var colorClass = `${currentCategory}Color`;
       timerButton.classList.add(colorClass);
     }
+
   }
 }
 
 function updateButton() {
   timerButton.innerText = `COMPLETE!`;
-  logActivityButton.classList.toggle("hidden");
+  logActivityButton.classList.toggle('hidden');
 }
 
 function startActivity() {
@@ -131,32 +123,33 @@ function startActivity() {
     unhide(currentActivity);
     activityTitle.innerText = 'Current Activity';
     chosenActivity.innerText = activity.description;
-    timeRemaining.innerText = `${(activity.minutes < 10 ? "0" : "") + activity.minutes}:${(activity.seconds < 10 ? "0" : "") + activity.seconds}`
+    timeRemaining.innerText = `${(activity.minutes < 10 ? '0' : '') + activity.minutes}:${(activity.seconds < 10 ? '0' : '') + activity.seconds}`;
   }
 };
 
 function startActivityTimer() {
   timerButton.disabled = true;
-  activity.startTimer();
-};
+  activity.startTimer(timeRemaining);
+}
 
 function logActivity() {
   hide(pastActivityDefault);
-  unhide(pastCardList);
   hide(currentActivity);
+  unhide(pastCardList);
   unhide(createNewActivityButton);
   activity.markComplete();
-  activityTitle.innerText = 'Completed Activity';
   activity.saveToStorage();
-  pastCardList.innerHTML +=
-  `<li class="past-activity-card">
-    <aside>
+  activityTitle.innerText = 'Completed Activity';
+  var listItem = document.createElement('li');
+  listItem.classList.add('past-activity-card')
+  listItem.innerHTML +=
+    `<aside>
         <p id="pastActivityTitle" class="past-activity-title">${activity.category}</p>
         <p id="pastActivityTime" class="past-activity-time">${activity.minutes} MIN</p>
         <p id="pastActivityGoal" class="past-activity-goal">${activity.description}</p>
      </aside>
-     <div class="past-activity-highlight ${activity.category}-highlight"></div>
-  </li>`;
+     <div class="past-activity-highlight ${activity.category}-highlight"></div>`;
+  pastCardList.prepend(listItem);
 };
 
 function createNewActivity() {
